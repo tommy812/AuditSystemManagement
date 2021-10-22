567d08d58418d3454fa81c2520945d362ed49197
Thomas Tamagni <TT449@student.aru.ac.uk>
2021-10-22 20:57:03 +0100

    gridview tables now working properly

--- SoftwareEngineeringApp/Forms/ListTemplatesForm.cs
+++ SoftwareEngineeringApp/Forms/ListTemplatesForm.cs
@@ -20,18 +20,14 @@ namespace SoftwareEngineeringApp.Forms
         public ListTemplatesForm()
         {
             InitializeComponent();
+
+            //Populate first gridview with the possible termplates
             DBConnection dbConn = DBConnection.getInstanceOfDBConnection();
             DataSet datasetTemplates = dbConn.GetData("SELECT * FROM Template");
             TemplateGrid.DataSource = datasetTemplates.Tables[0];
         }
 
-       // DataSet datasetTemplates = dbConn.GetData("SELECT Category FROM Categories WHERE Template_ID = " + template_no + "")
-
-               // using (SqlCommand cmd = new SqlCommand("SELECT Category FROM Categories WHERE Template_ID = " + template_no + "",con))
-
-
-        //   using (SqlCommand cmd = new SqlCommand("SELECT Question, Category_ID FROM Questions WHERE Template_ID = " + template_no + "", con))
-
+        //Open inspection form 
         private void iconButton1_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -41,7 +37,7 @@ namespace SoftwareEngineeringApp.Forms
 
         }
 
-
+        //populate all gridview with the questions contained in a selected template.
         private void TemplateGrid_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             CategoriesLbl.Visible = true;
@@ -50,11 +46,15 @@ namespace SoftwareEngineeringApp.Forms
             QuestionGrid.Visible = true;
             Int32.TryParse(TemplateGrid.Rows[e.RowIndex].Cells[0].Value.ToString(), out template_no);
             DBConnection dbConn = DBConnection.getInstanceOfDBConnection();
-            DataSet datasetCategory = dbConn.GetData("SELECT Category FROM Categories WHERE Template_ID = " + template_no + "");
-            DataSet datasetQuestions = dbConn.GetData("SELECT Question, Category_ID FROM Questions WHERE Template_ID = " + template_no + "");
+           // DataSet datasetCategory = dbConn.GetData("SELECT Category FROM Categories WHERE Template_ID = " + template_no + "");
+            DataSet datasetCategory = dbConn.GetData("select DISTINCT c.Category_ID, Category from Categories c  inner join Questions cl  on c.Category_ID = cl.Category_ID where cl.Template_ID = " + template_no + " ORDER BY c.Category_ID ASC;");
+            DataSet datasetQuestions = dbConn.GetData("SELECT Question, Category_ID FROM Questions WHERE Template_ID = " + template_no + " ORDER BY Category_ID ASC;");
             CategoryGrid.DataSource = datasetCategory.Tables[0];
             QuestionGrid.DataSource = datasetQuestions.Tables[0];
 
         }
     }
+  
+    
+    
 }
