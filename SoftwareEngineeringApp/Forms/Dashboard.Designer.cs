a6202b22db0f08afa391875eea760ac902cd221c
Thomas Tamagni <TT449@student.aru.ac.uk>
2021-11-10 17:27:47 +0000

    sd

--- SoftwareEngineeringApp/Forms/Dashboard.Designer.cs
+++ SoftwareEngineeringApp/Forms/Dashboard.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.Exit = new System.Windows.Forms.Button();
@@ -37,10 +40,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button8 = new System.Windows.Forms.Button();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ListGridView = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ListGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -49,9 +52,8 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1493, 172);
+            this.panel1.Size = new System.Drawing.Size(1120, 140);
             this.panel1.TabIndex = 2;
             // 
             // label1
@@ -61,10 +63,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label1.Location = new System.Drawing.Point(588, 57);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(441, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(454, 57);
+            this.label1.Size = new System.Drawing.Size(355, 44);
             this.label1.TabIndex = 0;
             this.label1.Text = "Admin Dashboard ";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -77,10 +78,9 @@
             this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Exit.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Exit.ForeColor = System.Drawing.Color.Gainsboro;
-            this.Exit.Location = new System.Drawing.Point(924, 837);
-            this.Exit.Margin = new System.Windows.Forms.Padding(4);
+            this.Exit.Location = new System.Drawing.Point(693, 680);
             this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(230, 87);
+            this.Exit.Size = new System.Drawing.Size(172, 71);
             this.Exit.TabIndex = 12;
             this.Exit.Text = "Exit";
             this.Exit.UseVisualStyleBackColor = false;
@@ -93,10 +93,9 @@
             this.btnWorkStnd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnWorkStnd.Font = new System.Drawing.Font("Century Gothic", 13F);
             this.btnWorkStnd.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnWorkStnd.Location = new System.Drawing.Point(980, 329);
-            this.btnWorkStnd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnWorkStnd.Location = new System.Drawing.Point(735, 267);
             this.btnWorkStnd.Name = "btnWorkStnd";
-            this.btnWorkStnd.Size = new System.Drawing.Size(237, 71);
+            this.btnWorkStnd.Size = new System.Drawing.Size(178, 58);
             this.btnWorkStnd.TabIndex = 13;
             this.btnWorkStnd.Text = "Statistics";
             this.btnWorkStnd.UseVisualStyleBackColor = false;
@@ -108,10 +107,9 @@
             this.btnSiteRule.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSiteRule.Font = new System.Drawing.Font("Century Gothic", 13F);
             this.btnSiteRule.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnSiteRule.Location = new System.Drawing.Point(283, 329);
-            this.btnSiteRule.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSiteRule.Location = new System.Drawing.Point(212, 267);
             this.btnSiteRule.Name = "btnSiteRule";
-            this.btnSiteRule.Size = new System.Drawing.Size(237, 71);
+            this.btnSiteRule.Size = new System.Drawing.Size(178, 58);
             this.btnSiteRule.TabIndex = 15;
             this.btnSiteRule.Text = "List of reports";
             this.btnSiteRule.UseVisualStyleBackColor = false;
@@ -119,12 +117,11 @@
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.flowLayoutPanel1.Controls.Add(this.dataGridView1);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(25, 417);
+            this.flowLayoutPanel1.Controls.Add(this.ListGridView);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(19, 339);
             this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(50);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(707, 405);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(530, 329);
             this.flowLayoutPanel1.TabIndex = 20;
             // 
             // label2
@@ -132,9 +129,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 18F);
             this.label2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label2.Location = new System.Drawing.Point(383, 228);
+            this.label2.Location = new System.Drawing.Point(287, 185);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(843, 37);
+            this.label2.Size = new System.Drawing.Size(700, 30);
             this.label2.TabIndex = 21;
             this.label2.Text = "Select one of the following options to generate a PDF file:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -146,10 +144,9 @@
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button8.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button8.Location = new System.Drawing.Point(648, 837);
-            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Location = new System.Drawing.Point(486, 680);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(222, 87);
+            this.button8.Size = new System.Drawing.Size(166, 71);
             this.button8.TabIndex = 22;
             this.button8.Text = "Back";
             this.button8.UseVisualStyleBackColor = false;
@@ -157,30 +154,64 @@
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(744, 417);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(558, 339);
             this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(50);
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(707, 405);
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(38, 41, 38, 41);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(530, 329);
             this.flowLayoutPanel2.TabIndex = 21;
             // 
-            // dataGridView1
+            // ListGridView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(53, 53);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(639, 322);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.ListGridView.AllowUserToAddRows = false;
+            this.ListGridView.AllowUserToDeleteRows = false;
+            this.ListGridView.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ListGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ListGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            this.ListGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ListGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ListGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.ListGridView.ColumnHeadersHeight = 30;
+            this.ListGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.ListGridView.EnableHeadersVisualStyles = false;
+            this.ListGridView.GridColor = System.Drawing.Color.SteelBlue;
+            this.ListGridView.Location = new System.Drawing.Point(3, 3);
+            this.ListGridView.Name = "ListGridView";
+            this.ListGridView.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ListGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.ListGridView.RowHeadersVisible = false;
+            this.ListGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            this.ListGridView.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.ListGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ListGridView.Size = new System.Drawing.Size(330, 254);
+            this.ListGridView.TabIndex = 8;
             // 
             // Dashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(58)))));
-            this.ClientSize = new System.Drawing.Size(1493, 951);
+            this.ClientSize = new System.Drawing.Size(1120, 773);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.btnSiteRule);
             this.Controls.Add(this.button8);
@@ -189,14 +220,13 @@
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.Exit);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Dashboard";
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ListGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,8 +242,8 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.DataGridView ListGridView;
     }
 }
 
--- SoftwareEngineeringApp/Forms/Dashboard.cs
+++ SoftwareEngineeringApp/Forms/Dashboard.cs
@@ -19,8 +19,9 @@ namespace SoftwareEngineeringApp
         {
             InitializeComponent();
             DBConnection dbConn = DBConnection.getInstanceOfDBConnection();
-            DataSet datasetTemplates = dbConn.GetData("select audit.audit_ID, audit.date, audit.completed_by, audit.site_ID, audit.area_ID, audit.supervisor_ID, audit.ispector_ID, audit.type_ID, audit.template_ID FROM site INNER JOIN audit.site_ID ON  = audit.site_ID INNER JOIN audit.area_ID ON = audit.area_ID INNER JOIN audit.inspector_ID ON = audit.inspector_ID INNER JOIN audit.type_ID ON = audit.type_ID INNER JOIN audit.template_ID ON = audit.template_ID ORDER BY audit_ID ASC");
-
+            DataSet datasetAudits = dbConn.GetData("Select a.Audit_ID, a.Date, a.Completed_By, s.Site_Name, w.Area, u.Name, t.type, te.template_name From audit_info a inner join Sites s on a.Site_ID = s.Site_ID inner join work_area w on a.Area_ID = w.Work_area_ID inner join users u on a.Inspector_ID = u.User_ID inner join Inspection_type t on a.Type_ID = t.type_ID inner join Template te on a.template_ID = te.template_ID ");
+            // DataSet datasetTemplates = dbConn.GetData("select audit.audit_ID, audit.date, audit.completed_by, audit.site_ID, audit.area_ID, audit.supervisor_ID, audit.ispector_ID, audit.type_ID, audit.template_ID FROM site INNER JOIN audit.site_ID ON  = audit.site_ID INNER JOIN audit.area_ID ON = audit.area_ID INNER JOIN audit.inspector_ID ON = audit.inspector_ID INNER JOIN audit.type_ID ON = audit.type_ID INNER JOIN audit.template_ID ON = audit.template_ID ORDER BY audit_ID ASC");
+            ListGridView.DataSource = datasetAudits.Tables[0];
         }
 
         
